039ca193caf3753f7c8f4392715020e238d64f15
sumeet-srknec <sumeet.srknec@gmail.com>
2024-09-20 07:30:20 +0100

    initial checkin

---
+++ backend/src/main/java/com/ssharma/cool_tasks/CoolTasksApplication.java
@@ -0,0 +1,13 @@
+package com.ssharma.cool_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolTasksApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssharma/cool_tasks/CoolTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssharma.cool_tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
